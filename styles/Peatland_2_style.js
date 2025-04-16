bb7f579bf644656673d36ae5b05ca6d20000a8ea
jamhurijamal90 <jamhuri.jamaluddin@gmail.com>
2025-04-16 12:45:05 +0800

    Add files via upload

--- styles/Peatland_2_style.js
+++ styles/Peatland_2_style.js
@@ -21,7 +21,7 @@ var style_Peatland_2 = function(feature, resolution){
         labelText = String("");
     }
     var style = [ new ol.style.Style({
-        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(178,223,138,1.0)'}),
+        stroke: new ol.style.Stroke({color: 'rgba(35,35,35,0.0)', lineDash: null, lineCap: 'butt', lineJoin: 'miter', width: 0.988}),fill: new ol.style.Fill({color: 'rgba(227,26,28,1.0)'}),
         text: createTextStyle(feature, resolution, labelText, labelFont,
                               labelFill, placement, bufferColor,
                               bufferWidth)
